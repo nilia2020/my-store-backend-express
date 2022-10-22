1dd87c68e6687f5c3b822a23153c4e9cb19ddc15
jorge niglia <jorge.niglia@gmail.com>
2022-10-22 19:04:47 -0300

    cors fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:5500/', 'http://127.0.0.1:5500/'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin) !== 1) {
+    if (whitelist.includes(origin) !== 1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
