ac212917569e65823f3d75201e83e4bccd40d030
jorge niglia <jorge.niglia@gmail.com>
2022-10-22 19:12:09 -0300

    procfile

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 // const { faker } = require('@faker-js/faker');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
